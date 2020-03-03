6b8cef784fe2be0df512c54cc6f893a176388950
Hadeel <hadeel.has97@gmail.com>
2020-03-04 00:46:59 +0300

    added react-native-firebase library

--- android/app/src/main/java/com/saudi_vibes/MainApplication.java
+++ android/app/src/main/java/com/saudi_vibes/MainApplication.java
@@ -3,9 +3,7 @@ package com.saudi_vibes;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
